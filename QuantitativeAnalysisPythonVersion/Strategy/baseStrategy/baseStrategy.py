9a173041fa804241aa1e9999db3b562b1036d84e
lidp <lidp@tradedesk>
2019-06-20 14:27:09 +0800

    更新策略

--- QuantitativeAnalysisPythonVersion/Strategy/baseStrategy/baseStrategy.py
+++ QuantitativeAnalysisPythonVersion/Strategy/baseStrategy/baseStrategy.py
@@ -4,6 +4,7 @@ from DataAccess.TradedayDataProcess import TradedayDataProcess
 from DataPrepare.tickFactorsProcess import tickFactorsProcess
 from Utility.JobLibUtility import JobLibUtility
 import pandas as pd
+import math
 from typing import List as type_list
 
 ########################################################################
@@ -24,6 +25,43 @@ class baseStrategy(object):
     #----------------------------------------------------------------------
     def strategy():
         raise NotImplementedError()
-
+    #----------------------------------------------------------------------
+    def buyByTickShotData(self,tick,myindex,targetPosition,priceDeviation=0):
+        ceilPrice=tick[myindex['S1']]*(1+priceDeviation)
+        buyPosition=0
+        buyAmount=0
+        for i in range(1,11):
+            price=tick[myindex['S'+str(i)]]
+            volume=tick[myindex['SV'+str(i)]]
+            if ((price<ceilPrice) & (targetPosition>0)):
+                buyVolume=min(targetPosition,volume)
+                buyPosition=buyPosition+buyVolume
+                buyAmount=buyAmount+buyVolume*price
+                targetPosition=targetPosition-buyVolume
+                pass
+            pass
+        pass
+        averagePrice=buyAmount/buyPosition
+        buyPosition=math.floor(buyPosition*0.01)*100
+        return [averagePrice,buyPosition]
+    #----------------------------------------------------------------------
+    def sellByTickShotData(self,tick,myindex,targetPosition,priceDeviation=0):
+        floorPrice=tick[myindex['B1']]*(1-priceDeviation)
+        sellPosition=0
+        sellAmount=0
+        for i in range(1,11):
+            price=tick[myindex['B'+str(i)]]
+            volume=tick[myindex['BV'+str(i)]]
+            if ((price>floorPrice) & (targetPosition>0)):
+                sellVolume=min(targetPosition,volume)
+                sellPosition=sellPosition+sellVolume
+                sellAmount=sellAmount+sellVolume*price
+                targetPosition=targetPosition-sellVolume
+                pass
+            pass
+        pass
+        averagePrice=sellAmount/sellPosition
+        sellPosition=math.floor(sellPosition*0.01)*100
+        return [averagePrice,sellPosition]
 ########################################################################
 
