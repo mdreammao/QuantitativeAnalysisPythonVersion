6f82e1e5e1fd4f4948a9d122f1144c57ba8bc7b3
lidp <lidp@tradedesk>
2019-06-28 14:05:01 +0800

    更新unittest

---
+++ QuantitativeAnalysisPythonVersion/Unitest/testFactor/testBuySellForce.py
@@ -0,0 +1,25 @@
+import unittest
+import os
+from Config.myConfig import *
+from DataPrepare.tickFactors.statusOfTickShot.buySellForce import buySellForce
+
+class testBuySellForce(unittest.TestCase):
+    """buySellForce因子的测试函数"""
+    def test_buySellForce(self):
+        for i in range(1000):
+            fileName=os.path.join(LocalFileAddress,'test','buySellForce','case'+str(i)+'.h5')
+            exists=os.path.isfile(fileName)
+            myinstance=buySellForce()
+            code='600000.SH'
+            date=20181231
+            if exists==True:
+                with pd.HDFStore(inputFile,'r',complib='blosc:zstd',append=True,complevel=9) as store:
+                    inputData=store['input']
+                    outputData=store['ouput']
+                    self.assertEqual(myinstance._buySellForce__computerFactor(code,date,inputData),outputData)
+                pass
+            pass
+        pass
+    pass
+
+
